4a91c9099fd2791a67c90f474a082faa3cff9aff
Ishu Mehta <mehta.ishu14@gmail.com>
2026-01-24 12:43:55 +0530

    setup endpoint for searching by tech

--- src/main/java/com/telusco/joblisting/controller/PostController.java
+++ src/main/java/com/telusco/joblisting/controller/PostController.java
@@ -5,12 +5,10 @@ import com.telusco.joblisting.repository.PostRepository;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -32,6 +30,31 @@ public class PostController {
     @GetMapping("/posts")
     public List<Post> getAllPosts() {
         return repository.findAll();
+    }
+
+    @GetMapping("/search")
+    public List<Post> search(@RequestParam String tech) {
+
+//        List<Post> result = new ArrayList<>();
+//        List<Post> allPosts = repository.findAll();
+//        for (Post post : allPosts) {
+//            for(String t: post.getTech()) {
+//                if(t.equalsIgnoreCase(tech)) {
+//                    result.add(post);
+//                    break;
+//                }
+//            }
+//        }
+//        return result;
+
+        return repository.findByTechIgnoreCase(tech);
+    }
+
+
+
+    @PostMapping("/create")
+    public Post createPost(@RequestBody Post post) {
+        return repository.save(post);
     }
 
 //    @GetMapping("/raw")
--- src/main/java/com/telusco/joblisting/repository/PostRepository.java
+++ src/main/java/com/telusco/joblisting/repository/PostRepository.java
@@ -2,8 +2,14 @@ package com.telusco.joblisting.repository;
 
 import com.telusco.joblisting.entity.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ tech: { $regex: ?0, $options: 'i' } }")
+    List<Post> findByTechIgnoreCase(String tech);
 }
